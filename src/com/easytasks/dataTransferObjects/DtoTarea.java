e9e563d6b68f543797283e286f274ee14357d350
aletocar <aletocar@hotmail.com>
2014-11-22 17:34:55 -0200

    Agregar Proyecto

--- src/com/easytasks/dataTransferObjects/DtoTarea.java
+++ src/com/easytasks/dataTransferObjects/DtoTarea.java
@@ -1,14 +1,20 @@
-
 package com.easytasks.dataTransferObjects;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class DtoTarea extends DtoRealizable{
+public class DtoTarea {
+
     private Date fechaCreacion;
     private Date fechaLimite;
-    private List<DtoRealizable> subtareas;
+    private List<DtoTarea> subtareas;
+    private Long id;
+    private String nombre;
+    private int prioridad;
+    private boolean completado;
+    private List<DtoUsuario> listaResponsables;
+    private List<DtoEtiqueta> listaEtiquetas;
 
     public Date getFechaCreacion() {
         return fechaCreacion;
@@ -26,15 +32,65 @@ public class DtoTarea extends DtoRealizable{
         this.fechaLimite = fechaLimite;
     }
 
-    public List<DtoRealizable> getSubtareas() {
+    public List<DtoTarea> getSubtareas() {
         return subtareas;
     }
 
-    public void setSubtareas(List<DtoRealizable> Subtareas) {
+    public void setSubtareas(List<DtoTarea> Subtareas) {
         this.subtareas = Subtareas;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public boolean isCompletado() {
+        return completado;
+    }
+
+    public void setCompletado(boolean completado) {
+        this.completado = completado;
+    }
+
+    public List<DtoUsuario> getListaResponsables() {
+        return listaResponsables;
+    }
+
+    public void setListaResponsables(List<DtoUsuario> listaResponsables) {
+        this.listaResponsables = listaResponsables;
+    }
+
+    public List<DtoEtiqueta> getListaEtiquetas() {
+        return listaEtiquetas;
+    }
+
+    public void setListaEtiquetas(List<DtoEtiqueta> listaEtiquetas) {
+        this.listaEtiquetas = listaEtiquetas;
+    }
+
     public DtoTarea() {
-        subtareas = new ArrayList<DtoRealizable>();
+        subtareas = new ArrayList<DtoTarea>();
+        listaEtiquetas = new ArrayList<DtoEtiqueta>();
+        listaResponsables = new ArrayList<DtoUsuario>();
     }
 }
